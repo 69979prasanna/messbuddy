6f6d1c8b8997b3f0ba1146f88e84f918c872052b
69979prasanna <prasannasolapure5@gmail.com>
2026-07-02 11:39:42 +0530

    Added Atlas and user schema

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password:{
+        type: String,
+        default:null,
+    },
+    authProvider:{
+        type: String,
+      enum: ["local", "google"],
+      default: "local"
+    },
+    googleId: {
+      type: String,
+      default: null
+    },
+
+    favorites: {
+      type: [Number],
+      default: []
+    },
+
+    preferences: {
+      diet: {
+        type: String,
+        default: null
+      },
+
+      budget: {
+        type: Number,
+        default: null
+      }
+    }
+  },
+  { timestamps: true }
+);
+export default mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import Groq from "groq-sdk";
 import { foodContext } from "./data/foodContext.js"
-
+import mongoose from "mongoose";
 dotenv.config();
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Mongo Error:", err));
 const app = express();
 app.use(cors());
 app.use(express.json());
