bc3952a857e17e34695baddd1189e4252f84faa8
69979prasanna <prasannasolapure5@gmail.com>
2026-07-03 10:02:04 +0530

    successfully added ATLAS and created authentication using bcrypt and jwt

---
+++ backend/routes/auth.js
@@ -0,0 +1,85 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import  dotenv from "dotenv";
+const router = express.Router();
+dotenv.config()
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields required"
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "Signup successful",
+      user: {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+});
+router.post("/login", async(req, res)=>{
+    try {
+        const {email, password} = req.body
+        const user  = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message:"Invalid Credential"})
+        }
+        const ismatch = await bcrypt.compare(password, user.password)
+        if(!ismatch){
+            return res.status(400).json({message:"Invalid Credential"})
+        }
+        const token  = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+
+        res.json({
+            message:"Login Successful",
+            token,
+            user:{
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+    
+    } catch (err) {
+        console.error(err);
+    res.status(500).json({
+      message: "Server error"
+    });
+    }
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import Groq from "groq-sdk";
 import { foodContext } from "./data/foodContext.js"
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
@@ -12,7 +13,7 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
 });
